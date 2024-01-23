9051c493f52bf7c28eb01926d128272d2ccd5388
averywh13 <awekslerhart@gmail.com>
2024-01-23 14:55:14 -0500

    worked on javascript

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // function to calculate and display tip, total bill, and amount per person
 function calculateAndDisplay() {
+    // get the values from the html form
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPeople = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+
+    // calculate tip, total bill, amount each person owes
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPerson = calcAmtPerPerson(totalBill, numOfPeople)
     
+    // display the results in the html
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amtPerPerson.toFixed(2)
 }
